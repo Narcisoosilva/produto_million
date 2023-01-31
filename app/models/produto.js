51aeb7ad2cba3fb76a210921c9a45aebc371c2fb
Narcisoosilva <narcisoosilva@gmail.com>
2023-01-31 18:56:18 -0300

    Criando teste de post, teste funcionando

--- app/models/produto.js
+++ app/models/produto.js
@@ -3,7 +3,7 @@ var mongoose = require('../../db/conexao');
 const Produto = mongoose.model('user', {
   nome: {type: String, required: true, unique:true },
   descricao:{type: String, required: true },
-  nivel_investidor:{type: Number, required: true },
+  nivel_investidor:{type: String, required: true },
 });
 
 module.exports = Produto;
--- spec/controllers/produto_controllers_spec.js
+++ spec/controllers/produto_controllers_spec.js
@@ -2,27 +2,44 @@ const axios = require('axios').default;
 const request = require("request-promise");
 const Produto = require('../../app/models/produto');
 const host = "http://localhost:3000";
+const TOKEN = "123456789";
 
 describe("AmdController", () =>{
-    beforeEach(async() =>{
-      await Produto.deleteMany({descricao: /test/}).then(error =>{}); 
-      await Produto.create([{nome: "teste1", descricao:'test do produto1', nivel_investidor: 3 }, {nome: "teste2", descricao:'test do produto2', nivel_investidor: 5 }]);
+  beforeEach(async() =>{
+    await Produto.deleteMany({descricao: /es/}).then(error =>{}); 
+    await Produto.create([{nome: "teste1", descricao:'test do produto1', nivel_investidor: 3 }, {nome: "teste2", descricao:'test do produto2', nivel_investidor: 5 }]);
+  });
+
+
+  describe("GET /produto.json - deve retornar uma lista de produtos", () =>{
+    it("deve retornar o status code de 200", async(done) => {
+      const response = await axios.get(`${host}/produto.json`, {headers: {'token': TOKEN}});
+      expect(response.status).toBe(200);
+      done();
     });
-  
-  
-    describe("GET /produto.json - deve retornar uma lista de produtos", () =>{
-      it("deve retornar o status code de 200", async(done) => {
-        const response = await axios.get(`${host}/produto.json`);
-        expect(response.status).toBe(200);
-        done();
-      });
-      
-      it("deve retornar dados do Produtos da API", async(done) => {
-        const response = await axios.get(`${host}/produto.json`);
-        const itens = response.data;
-        expect(itens[0].nome).toBe("teste1");
-        expect(itens[1].nome).toBe("teste2");
-        done();
-      });
+    
+    it("deve retornar dados do Produtos da API", async(done) => {
+      const response = await axios.get(`${host}/produto.json`, {headers: {'token': TOKEN}});
+      const itens = response.data;
+      expect(itens[0].nome).toBe("teste1");
+      expect(itens[1].nome).toBe("teste2");
+      done();
     });
+  });
+
+  describe("POST /produto.json - produto", () =>{
+    it("deve cadastrar um produto na API", async(done) => {
+      let nome = `teste ${new Date().getTime()}`;
+      const body = { 
+        nome, 
+        descricao: "Alguma coisa falando sobre o produto",
+        nivel_investidor: "5"
+      }
+      const response = await axios.post(`${host}/produto.json`, body, {headers: {'token': TOKEN}});
+      expect(response.status).toBe(201);
+      done();
+    });
+  });
+
+  
 });
